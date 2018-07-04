01cd47f8d7a5a2f8c701c8cae84f5e9a5c874b22
Mosmostafa abd elrasool <mmr@crossworkers.com>
2018-07-04 11:31:21 +0200

    PropertyExtention

---
+++ MKMapper/MKMapper/PropertyExtention.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKMapper
+{
+    public static class PropertyExtention
+    {
+        public static string GetPropertyName(this object property)
+        {
+            property.GetType();
+            return "";
+        }
+    }
+}
